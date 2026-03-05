a8f2b68dc7bbbad8e7fc8b0bbb7f9f9b28f6f96e
0215-eren <262452167+0215-eren@users.noreply.github.com>
2026-03-05 13:30:32 +0000

    My first commit for this project, i didn't finish exercice 1 yet, but this was a test

---
+++ exercice1.js
@@ -0,0 +1,11 @@
+const a = 42;
+const b = "Boujour";
+const c = true;
+const d = null;
+let e;
+const f = [1, 2, 3];
+const g = {nom: "Ali"};
+const h = NaN;
+
+console.log(`${a} -> ${typeof a}`);
+
